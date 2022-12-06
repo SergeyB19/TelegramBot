8b57930ce063c60607903bbadca0b61c808c400a
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-12-06 17:09:15 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course3/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
