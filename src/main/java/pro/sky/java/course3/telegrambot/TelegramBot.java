5a749ec2320bc12e7f1a981375e4762736b69437
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-12-07 22:09:20 +0300

    course work

--- src/main/java/pro/sky/java/course3/telegrambot/TelegramBot.java
+++ src/main/java/pro/sky/java/course3/telegrambot/TelegramBot.java
@@ -1,11 +1,39 @@
 package pro.sky.java.course3.telegrambot;
 
+import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TelegramBot {
+    private String messageText;
+    private Object chatId;
     public TelegramBot(String token) {
     }
 
     public void execute(DeleteMyCommands deleteMyCommands) {
+    }
+
+    TelegramBot bot = new TelegramBot("BOT_TOKEN");
+
+    bot.setUpdatesListener(updates -> {
+        // Обрабатываем входящие сообщения
+        // возвращаем id последнего обработанного сообщения или отмечаем все как обработанные
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    });
+
+
+    SendMessage message = new SendMessage(chatId, messageText);
+
+    SendResponse response = bot.execute(message);
+    Pattern pattern = Pattern.compile("[a-zA-Z]");
+
+    Matcher matcher = pattern.matcher(string);
+if (matcher.matches()) {
+        String date = matcher.group(1);
+        String item = matcher.group(3);
     }
 }
--- src/main/java/pro/sky/java/course3/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/java/course3/telegrambot/TelegramBotApplication.java
@@ -3,6 +3,7 @@ package pro.sky.java.course3.telegrambot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -14,6 +15,12 @@ public class TelegramBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotApplication.class, args);
         LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+
     }
+    @Scheduled(fixedDelay = 1_000L)
+    public void run() {
+    }
+
 
 }
