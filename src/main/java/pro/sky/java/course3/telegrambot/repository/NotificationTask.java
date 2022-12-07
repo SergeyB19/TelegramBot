16bf299e856e653773479dd3fd1eb3d9d8eedc92
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-12-07 21:57:44 +0300

    course work

--- src/main/java/pro/sky/java/course3/telegrambot/repository/NotificationTask.java
+++ src/main/java/pro/sky/java/course3/telegrambot/repository/NotificationTask.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Entity;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
 @Repository
+@Entity
 public class NotificationTask implements JpaRepository {
     @Override
     public Object save(Object entity) {
